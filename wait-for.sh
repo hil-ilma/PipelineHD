fc9ea97c82064806557875db61b77207af5006bf
Hilma <hilmaazmy@gmail.com>
2025-05-26 12:09:55 +1000

    fix: converted SQL files to Unix format for Docker compatibility

--- wait-for.sh
+++ wait-for.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-echo " Waiting for MySQL at $DB_HOST:$DB_PORT..."
 
-until nc -z "$DB_HOST" "$DB_PORT"; do
-  >&2 echo " MySQL is unavailable - sleeping"
+echo "⏳ Waiting for MySQL at $DB_HOST:$DB_PORT..."
+
+until nc -z "$DB_HOST" "$DB_PORT"; do     
+  >&2 echo "❌ MySQL is unavailable - sleeping"
   sleep 2
 done
 
-echo " MySQL is up - executing command"
+echo "✅ MySQL is up - executing command"
 exec "$@"
